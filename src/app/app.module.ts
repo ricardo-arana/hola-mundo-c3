f9279805da8fc2536f5e9f9122584683c455c0db
Ricardo Arana Reyes <raranare@everis.com>
2020-12-11 12:38:36 -0500

    ng for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './components/hola-mundo/hola-mundo.component';
 import { SaludadorComponent } from './components/saludador/saludador.component';
+import { ListaComponent } from './components/lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HolaMundoComponent,
-    SaludadorComponent
+    SaludadorComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/lista/lista.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+  items: string[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregar(item: string) {
+    this.items.push(item);
+    console.log(this.items);
+  }
+
+}
